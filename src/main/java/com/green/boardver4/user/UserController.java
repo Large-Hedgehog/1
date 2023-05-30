7cbe3ba6451cc16c59322230676eaaa9f5d93ef4
김다율 <kdy12061004@gmail.com>
2023-05-30 12:36:04 +0900

    2023-05-30 : 폴더 추가-김다율

--- src/main/java/com/green/boardver4/user/UserController.java
+++ src/main/java/com/green/boardver4/user/UserController.java
@@ -1,5 +1,6 @@
 package com.green.boardver4.user;
 import com.green.boardver4.user.model.UserInsDto;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,13 @@ public class UserController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "회원가입", description = ""+
+            "uid\": [20] 회원 아이디\", <br>"+
+            "upw\": [100] 회원 비밀번호\", <br>"+
+            "nm\": [30] 회원 이름\", <br>"+
+            "gender\": [1] m:남자 f:여자 \", <br>"+
+            "addr\": [100] 주소")
     public int boardPost(@RequestBody UserInsDto dto) {
         return service.UserIns(dto);
     }
