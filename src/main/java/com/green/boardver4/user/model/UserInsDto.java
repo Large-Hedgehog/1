a9faf3fb267918aa0ad3f22615fa3cdd28314f76
서영기 <matep8@knou.ac.kr>
2023-05-30 11:01:07 +0900

    2023-05-30 DB회원가입-서영기

--- src/main/java/com/green/boardver4/user/model/UserInsDto.java
+++ src/main/java/com/green/boardver4/user/model/UserInsDto.java
@@ -2,5 +2,10 @@ package com.green.boardver4.user.model;
 import lombok.Data;
 @Data
 public class UserInsDto {
-
+    private String uid;
+    private String upw;
+    private String nm;
+    private Character gender;
+    private String addr;
+    private String main_pic;
 }
