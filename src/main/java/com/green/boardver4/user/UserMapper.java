5993bd9d6de7bdeace50b6504e7dd24080187ac3
서영기 <matep8@knou.ac.kr>
2023-05-30 14:59:01 +0900

    2023-05-30 LoginVo-서영기

--- src/main/java/com/green/boardver4/user/UserMapper.java
+++ src/main/java/com/green/boardver4/user/UserMapper.java
@@ -5,5 +5,5 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
     int UserIns(UserInsDto dto);
-    int UserSel(UserLoginDto dto);
+    int selUserByUid(UserLoginDto dto);
 }
