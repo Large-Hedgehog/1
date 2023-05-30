66131e81f94cb2c7f8e01abee0aa616036d3c78d
서영기 <matep8@knou.ac.kr>
2023-05-30 14:36:27 +0900

    2023-05-30 DB회원가입-서영기

--- src/main/java/com/green/boardver4/user/UserMapper.java
+++ src/main/java/com/green/boardver4/user/UserMapper.java
@@ -1,7 +1,9 @@
 package com.green.boardver4.user;
 import com.green.boardver4.user.model.UserInsDto;
+import com.green.boardver4.user.model.UserLoginDto;
 import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
     int UserIns(UserInsDto dto);
+    int UserSel(UserLoginDto dto);
 }
--- src/main/java/com/green/boardver4/user/model/UserLoginDto.java
+++ src/main/java/com/green/boardver4/user/model/UserLoginDto.java
@@ -1,4 +1,6 @@
 package com.green.boardver4.user.model;
 
 public class UserLoginDto {
+    private String uid;
+    private String upw;
 }
