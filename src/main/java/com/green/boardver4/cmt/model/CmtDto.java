321b4e7b17f2290a577381003ad70fde9a138de5
황주은 <rlahfld54@naver.com>
2023-06-01 11:00:15 +0900

    2023-05-30 댓글 select DB - 황주은

--- src/main/java/com/green/boardver4/cmt/model/CmtDto.java
+++ src/main/java/com/green/boardver4/cmt/model/CmtDto.java
@@ -3,11 +3,12 @@ package com.green.boardver4.cmt.model;
 import lombok.Data;
 
 @Data
-public class CmtEntity {
+public class CmtDto {
     private int iboardCmt;
     private int iboard;
     private int iuser;
+    private String writer;
+    private String writerMainPic;
     private String ctnt;
     private String createdAt;
-    private String updatedAt;
 }
